737695a381a48943b9f9bced8a55c33f30fb8d03
sunny kumar <sunnykr3913@gmail.com>
2024-08-21 02:24:49 +0530

    update stuff

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
+import { metadata } from "@/app/layout"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+  metadataBase: new URL("https://sheet-cp.vercel.app/");
 }
